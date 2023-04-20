a12d81624ff3b124251edece651fc13ef673c731
Sri Panyam <sri.panyam@gmail.com>
2023-04-19 18:38:17 -0700

    Some good tree printing

--- dagcli/utils.py
+++ dagcli/utils.py
@@ -18,32 +18,66 @@ def present(ctx: typer.Context, results):
         if results:
             tree = {}
             if type(results) in (bool, str, int, float):
-                print(results)
+                pprint(results)
+            elif not ctx.obj.tree_transformer:
+                pprint(results)
+                # set_trace()
+                # assert False, "'tree' output format needs a tree transformer to convert results into a tree structure where each node only has either 'title' or 'children'"
             else:
-                if not ctx.obj.tree_transformer:
-                    pprint(results)
-                    set_trace()
-                    assert False, "'tree' output format needs a tree transformer to convert results into a tree structure where each node only has either 'title' or 'children'"
                 tree = ctx.obj.tree_transformer(results)
-                output = render(tree)
-                lines = [(level * " ") + p + (" ") + title if p else title for (p,title,level) in output]
+                lines = render_tree(tree)
                 print("\n".join(lines))
     else:
         # Apply a result type specific transformer here
         print("Invalid output format: ", ctx.obj.output_format)
 
-def render(root, lines=None, level=0, indent=4):
+def make_tree(prefix, num_levels, num_children_per_level):
+    root = {"title": prefix, "children": []}
+    if num_levels > 0:
+        for i in range(num_children_per_level):
+            root["children"].append(make_tree(f"{prefix}.{i}", num_levels - 1, num_children_per_level))
+    return root
+
+t1 = make_tree("Root", 0, 0)
+t2 = make_tree("Node", 3, 3)
+
+def render_tree(root, indent=4):
+    output = render_node(root, [], 0, indent=indent)
+    lines = [((level - 1) * " ") + sp + lp + (" ") + title if lp else title for (sp, lp, title,level) in output]
+    # lines = [sp + lp + (" ") + title if lp else title for (sp, lp, title,level) in output]
+    def pipeindex(level, indent=4):
+        """ Given an indent size and "level" returns the index of where the starting "|" would occur """
+
+    def setchar(s, index, ch):
+         return s[:index] + ch + s[index+1:]
+
+    for index, (sp, lp, title, level) in enumerate(output):
+        if level > 0:
+            pi = len(sp) + level - 1
+            lines[index] = setchar(lines[index], pi, "\u2517")
+            for nextind in range(index - 1, 0, -1):
+                _,_,_,nextlevel = output[nextind]
+                if nextlevel > level:
+                    lines[nextind] = setchar(lines[nextind], pi, "\u2503")
+                elif nextlevel == level:
+                    lines[nextind] = setchar(lines[nextind], pi, "\u2523")
+                else:
+                    break
+    return lines
+
+def render_node(root, lines=None, level=0, indent=4):
     indentstr = " " * indent
     if lines is None: lines = []
     if root:
-        prefix = ""
+        space_prefix = ""
+        line_prefix = ""
         if level > 0:
-            prefix += (level - 1) * indentstr
-            prefix += "  |" + (indent - 1) * "-"
-        if type(root) is str:
-            lines.append((prefix, root, level))
-        else:
-            lines.append((prefix, root["title"], level))
+            space_prefix = (level - 1) * indentstr
+            line_prefix = "|" + (indent - 1) * "\u2501"
+        title = root if type(root) is str else root["title"]
+        lines.append((space_prefix, line_prefix, title, level))
+
+        if type(root) is not str:
             for child in root.get("children", []):
-                lines = render(child, lines, level + 1, indent)
+                lines = render_node(child, lines, level + 1, indent)
     return lines
