b3be350d4dac6939689f4ddd0a9c5d81f06157ac
Sri Panyam <sri.panyam@gmail.com>
2024-02-16 14:12:23 -0800

    latest

--- dagcli/proxy.py
+++ dagcli/proxy.py
@@ -138,7 +138,12 @@ def delete(ctx: typer.Context, label: str = typer.Argument(..., help="Label of t
 def initk8s(ctx: typer.Context,
             env_file: typer.FileText = typer.Argument("./.env", help = "Env file for your proxy.  If you do not have one then run the `dk getenv` command"),
             dest_dir: str = typer.Argument(None, help = "Destination folder where k8s files will be generated for your proxy.  If not provided `./proxies/<PROXY_ALIAS>` will be used"),
+            storage_type: str = typer.Option("local", help = "Storage type - options: 'local', 'efs'"),
+            efs_fs_id: str = typer.Option("", help = "Filesystem ID of the EFS volume if using EFS for storage"),
+            eks_cluster_name: str = typer.Option("", help = "Name of the EKS cluster to run against.  Will be needed if storage_type is efs"),
+            aws_region: str = typer.Option("us-east-2", help = "Region to use if using AWS"),
             local_pv_root: str = typer.Option(None, help = "Root folder of the local PVs.  Will default to '<dest_dir>/localpv'"),
+            minikube_mount_root: str = typer.Option("/minikubemount", help = "Folder on the Minikube VM where the storage will be mounted to local_pv_root on"),
             sidecar_root: str = typer.Option("/app/sidecar", help = "Root folder for our sidecar libs"),
             pyrunner_root: str = typer.Option(None, help = "Root folder for pyrunner related files.  Will default to <sidecar_root>/pyrunner"),
             pyenv_root: str = typer.Option(None, help = "Root folder for pyenv related files.  Will default to <pyrunner_root>/pyenv"),
@@ -172,13 +177,32 @@ def initk8s(ctx: typer.Context,
     envvars["SIDECAR_ROOT"] = sidecar_root
     envvars["PYENV_ROOT"] = pyenv_root
     envvars["PYRUNNER_ROOT"] = pyrunner_root
+    envvars["PROXY_NAMESPACE"] = k8s_namespace
+    envvars["LOCAL_PV_ROOT"] = local_pv_root
+    envvars["MINIKUBE_MOUNT_ROOT"] = minikube_mount_root
+    envvars["EFS_FILESYSTEM_ID"] = efs_fs_id
+    envvars["EKS_CLUSTER_NAME"] = eks_cluster_name
+    envvars["AWS_REGION"] = aws_region
+
+    if storage_type == "efs":
+        assert efs_fs_id not in ("", None), "efs_fs_id param must be specified if storage type is efs"
+        assert eks_cluster_name not in ("", None), "eks_cluster_name param must be specified if storage type is efs"
+        assert aws_region not in ("", None), "aws_region param must be specified if storage type is efs"
 
     for tmplfile in os.listdir("./templates"):
+        if tmplfile in ("storage",): continue
         tf = open(os.path.join("./templates", tmplfile)).read()
         ofpath = os.path.join(dest_dir, tmplfile)
         with open(ofpath, "w") as outfile:
-            tf = tf.replace("{{PROXY_NAMESPACE}}", k8s_namespace)
-            tf = tf.replace("{{LOCAL_PV_ROOT}}", local_pv_root)
+            for k,v in envvars.items():
+                tf = tf.replace("{{" + k + "}}", v)
+            outfile.write(tf)
+
+    # Storage specific things
+    for tmplfile in os.listdir(f"./templates/storage/{storage_type}"):
+        tf = open(os.path.join("./templates/storage", storage_type, tmplfile)).read()
+        ofpath = os.path.join(dest_dir, tmplfile)
+        with open(ofpath, "w") as outfile:
             for k,v in envvars.items():
                 tf = tf.replace("{{" + k + "}}", v)
             outfile.write(tf)
@@ -186,3 +210,6 @@ def initk8s(ctx: typer.Context,
     # Save the updated env file too
     with open(os.path.join(dest_dir, ".env"), "w") as outenvfile:
         outenvfile.write("\n".join([f"{k}={v}" for k,v in envvars.items()]))
+
+    # Save daglib too
+    # with open(os.path.join(dest_dir, "daglib.py"), "w") as daglibfile: daglibfile.write(open("../outpost/jobs/daglib.py").read())
