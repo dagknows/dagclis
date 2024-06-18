19ad3e7bc9810a08de2c85d47c8d27741941798a
Sri Panyam <sri.panyam@gmail.com>
2024-06-18 15:16:05 -0700

    Adding a keys command

--- dagcli/vlt.py
+++ dagcli/vlt.py
@@ -17,6 +17,9 @@ app.add_typer(ips.app, name="ips", callback=ensure_mandatory)
 from dagcli.vault import urls
 app.add_typer(urls.app, name="urls", callback=ensure_mandatory)
 
+from dagcli.vault import keys
+app.add_typer(keys.app, name="keys", callback=ensure_mandatory)
+
 from dagcli.vault import hostgroups
 app.add_typer(hostgroups.app, name="hostgroups", callback=ensure_mandatory)
 
